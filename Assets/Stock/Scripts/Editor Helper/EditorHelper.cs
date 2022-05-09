fb26192e91b6a093606241ffd42d17c00c1ccb46
AdamZkingspleasure <adam.zadorski@kingspleasure.com>
2022-05-09 16:03:56 +0200

    Editor helper upgrades

--- Assets/Stock/Scripts/Editor Helper/EditorHelper.cs
+++ Assets/Stock/Scripts/Editor Helper/EditorHelper.cs
@@ -13,8 +13,12 @@ public class EditorHelper : OdinEditorWindow
 {
     private bool showPath;
     private bool showInteractiveItems;
+    
     public GameObject floorPrefab;
     public Vector2 gridSize;
+
+    public GameObject wallPrefab;
+    public int wallLenght;
 
     [MenuItem("EditorTools/OpenTools")]
     private static void OpenWindow()
@@ -25,41 +29,120 @@ public class EditorHelper : OdinEditorWindow
     [Button("Spawn Grid")]
     private void SpawnFloorGrid()
     {
+        GameObject floorParrent = new GameObject("Floor Parrent");
+       
         for (int x = 0; x < gridSize.x; x++)
         {
             for (int y = 0; y < gridSize.y; y++)
             {
-                GameObject instantiadtedGrid = Instantiate(floorPrefab);
+                GameObject instantiadtedGrid = Instantiate(floorPrefab, floorParrent.transform);
                 instantiadtedGrid.transform.position = new Vector3(x * 2.5f, 0, y * 2.5f);
             }
         }
     }
+    [Button("Spawn Wall")]
+    private void SpawnWall()
+    {
+        GameObject wallParrent = new GameObject("Wall Parrent");
 
-
-
-
-        [Button("Show Interactive Items")]
-        private void ShowInteractiveItems()
-        {
-            if (showInteractiveItems) showInteractiveItems = false; else showInteractiveItems = true;
-
-            PickablePoint[] components = GameObject.FindObjectsOfType<PickablePoint>();
-            foreach (var item in components)
+            for (int i = 0; i < wallLenght; i++)
             {
-                item.InteractivePointMesh.SetActive(showInteractiveItems);
+                GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+                instantiadtedGrid.transform.position = new Vector3(i * 2.5f, 0, 0);
+            }
+    }
+
+    [Button("Spawn Room")]
+    private void SpawnRoom()
+    {
+        GameObject room = new GameObject("Room Parrent");
+
+
+        GameObject floorParrent = new GameObject("Floor Parrent");
+        floorParrent.transform.SetParent(room.transform);
+        for (int x = 0; x < gridSize.x; x++)
+        {
+            for (int y = 0; y < gridSize.y; y++)
+            {
+                GameObject instantiadtedGrid = Instantiate(floorPrefab, floorParrent.transform);
+                instantiadtedGrid.transform.position = new Vector3(x * 2.5f, 0, y * 2.5f);
             }
         }
 
-        [Button("Show Maze Colliders")]
-        private void ShowMazeColliders()
+        GameObject wallParrent = new GameObject("Wall Parrent");
+        wallParrent.transform.SetParent(room.transform);
+        for (int i = 0; i < gridSize.y; i++)
         {
-            if (showPath) showPath = false; else showPath = true;
+            GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+            instantiadtedGrid.transform.position = new Vector3(-2.5f, 0, i * 2.5f);
+            instantiadtedGrid.transform.eulerAngles = new Vector3(0, 90, 0);
+        }
 
-            DrawPathGizmos[] components = GameObject.FindObjectsOfType<DrawPathGizmos>();
-            foreach (var item in components)
-            {
-                item.showPath = showPath;
-            }
+        for (int i = 0; i < gridSize.y; i++)
+        {
+            GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+            instantiadtedGrid.transform.position = new Vector3((gridSize.x * 2.5f) - 2.5f, 0, i * 2.5f);
+            instantiadtedGrid.transform.eulerAngles = new Vector3(0, 90, 0);
+        }
+
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+            instantiadtedGrid.transform.position = new Vector3((i * 2.5f), 0, 0);
+            instantiadtedGrid.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        for (int i = 0; i < gridSize.x; i++)
+        {
+            GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+            instantiadtedGrid.transform.position = new Vector3((i * 2.5f), 0, gridSize.y * 2.5f);
+            instantiadtedGrid.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        //for (int i = 0; i < gridSize.y; i++)
+        //{
+        //    GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+        //    instantiadtedGrid.transform.position = new Vector3(0, 0, (i * 2.5f) + 2.5f);
+        //    instantiadtedGrid.transform.eulerAngles = new Vector3(0, 90, 0);
+        //}
+
+        //for (int i = 0; i < gridSize.y; i++)
+        //{
+        //    GameObject instantiadtedGrid = Instantiate(wallPrefab, wallParrent.transform);
+        //    instantiadtedGrid.transform.position = new Vector3(1* gridSize.y, 0, (i * 2.5f) + 2.5f);
+        //    instantiadtedGrid.transform.eulerAngles = new Vector3(0, 90, 0);
+        //}
+
+
+    }
+
+
+
+
+
+
+    [Button("Show Interactive Items")]
+    private void ShowInteractiveItems()
+    {
+        if (showInteractiveItems) showInteractiveItems = false; else showInteractiveItems = true;
+
+        PickablePoint[] components = GameObject.FindObjectsOfType<PickablePoint>();
+        foreach (var item in components)
+        {
+            item.InteractivePointMesh.SetActive(showInteractiveItems);
         }
     }
+
+    [Button("Show Maze Colliders")]
+    private void ShowMazeColliders()
+    {
+        if (showPath) showPath = false; else showPath = true;
+
+        DrawPathGizmos[] components = GameObject.FindObjectsOfType<DrawPathGizmos>();
+        foreach (var item in components)
+        {
+            item.showPath = showPath;
+        }
+    }
+}
 #endif
