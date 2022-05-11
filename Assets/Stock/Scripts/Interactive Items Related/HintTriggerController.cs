cdff432cc67e3f5d7bc8fc89560f794ff4fd952e
AdamZkingspleasure <adam.zadorski@kingspleasure.com>
2022-05-11 20:51:07 +0200

    Added infobox with available conditions

--- Assets/Stock/Scripts/Interactive Items Related/HintTriggerController.cs
+++ Assets/Stock/Scripts/Interactive Items Related/HintTriggerController.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Sirenix.OdinInspector;
 public class HintTriggerController : MonoBehaviour
 {
-    public string title;
-    public string constents;
+    public string title = "Example Title";
+    public string constents = "Example Constents";
     public Sprite icon;
-    public string conditionToClose;
-    public bool lockPlayerMovement;
-
+    [InfoBox("Left, Right, Down, Up, Tab")]
+    public string conditionToClose = "Left";
+    public bool lockPlayerMovement = true;
+    public bool destroyAfterEnter = true;
     public void OnHintTriggerEnter()
     {
         PopupController.Instance.OpenPopup(title, constents, icon, conditionToClose, lockPlayerMovement);
-        Destroy(this.gameObject);
+        if (destroyAfterEnter)
+        {
+            Destroy(this.gameObject);
+        }
+
     }
 }
