89812d2bae394caaa7b498e3afb8a3f318461dfa
evilferber <evilferber@gmail.com>
2022-05-16 18:48:53 +0200

    Changed "Tab" to "Tap" in InfoBox of the Script Inversed lockPlayerMovement behaviour to work as expected.

--- Assets/Stock/Scripts/Interactive Items Related/HintTriggerController.cs
+++ Assets/Stock/Scripts/Interactive Items Related/HintTriggerController.cs
@@ -7,13 +7,13 @@ public class HintTriggerController : MonoBehaviour
     public string title = "Example Title";
     public string constents = "Example Constents";
     public Sprite icon;
-    [InfoBox("Left, Right, Down, Up, Tab")]
+    [InfoBox("Left, Right, Down, Up, Tap")]
     public string conditionToClose = "Left";
     public bool lockPlayerMovement = true;
     public bool destroyAfterEnter = true;
     public void OnHintTriggerEnter()
     {
-        PopupController.Instance.OpenPopup(title, constents, icon, conditionToClose, lockPlayerMovement);
+        PopupController.Instance.OpenPopup(title, constents, icon, conditionToClose, !lockPlayerMovement);
         if (destroyAfterEnter)
         {
             Destroy(this.gameObject);
